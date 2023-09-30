00b49cc4257f54bf74389946d432f6d1367b42b8
Manoj-k <manojkrishnappa7@gmal.com>
2023-09-30 13:19:58 +0000

    mini-kube doc is created

---
+++ minikube-setup/virtualbox-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+sudo apt-get update -y
+sudo apt-get install conntrack -y
+sudo apt-get install curl apt-transport-https wget -y
+sudo apt-get install virtualbox virtualbox-ext-pack -y
+
